5746569c75a66b5ae53a3e5183b2eddf95a20785
Alexander Steenbergen <alexandersteenbergen@gmail.com>
2022-04-22 10:19:36 +0200

    Fix offsets

--- src/Developers.js
+++ src/Developers.js
@@ -9,7 +9,7 @@ import Paper from '@mui/material/Paper';
 
 const monthLenghts = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-const startDayOfThreeWeekSprints = 103
+const startDayOfThreeWeekSprints = 104
 const devIndexOnStartOfThreeWeeksSprint = 4
 
 function daysSinceStartOfYear(date) {
