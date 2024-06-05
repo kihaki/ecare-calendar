8a639556b87776fdbaf2263c3e68e1d1d33cac82
Alexander Steenbergen <alexandersteenbergen@gmail.com>
2024-06-05 14:20:10 +0200

    Forwar/Backward Buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './App.css';
 import Developers from './Developers.js';
 import DatePicker from "react-datepicker";
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import "react-datepicker/dist/react-datepicker.css";
 
 const devs = ['Alex', 'Christian', 'Florian', 'Hung', 'Jochen', 'Michael', 'Simon', 'Sven']
@@ -10,7 +13,13 @@ function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   return (
     <div className="App">
+      <IconButton aria-label="back" onClick={() => setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate() - 1)))}>
+        <ArrowBackIcon />
+      </IconButton>
       <DatePicker dateFormat="yyyy-MM-dd" selected={selectedDate} onChange={(date) => setSelectedDate(date)} />
+      <IconButton aria-label="forward" onClick={() => setSelectedDate(new Date(selectedDate.setDate(selectedDate.getDate() + 1)))}>
+        <ArrowForwardIcon />
+      </IconButton>
       <Developers devs={devs} date={selectedDate} />
     </div>
   );
